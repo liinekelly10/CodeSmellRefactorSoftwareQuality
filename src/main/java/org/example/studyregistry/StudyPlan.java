91c5184c596806db163d0d33e3210551a38dbd9d
liinekelly10 <alinekellysl61@gmail.com>
2025-07-07 00:14:16 -0300

    fix: refactor code smell Excessive Parameter List using refactoringTechniqueName Introduce Parameter Object

--- src/main/java/org/example/studyregistry/StudyPlan.java
+++ src/main/java/org/example/studyregistry/StudyPlan.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class StudyPlan extends Registry{
+public class StudyPlan extends Registry {
     private StudyObjective objective;
     private List<String> steps;
 
@@ -17,7 +17,7 @@ public class StudyPlan extends Registry{
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Plan: " + name + ",\nObjective: " + objective.getDescription() + ",\nSteps: " + String.join(", ", steps);
     }
 
@@ -33,7 +33,7 @@ public class StudyPlan extends Registry{
         this.objective = objective;
     }
 
-    public void addSingleStep(String toAdd){
+    public void addSingleStep(String toAdd) {
         steps.add(toAdd);
     }
 
@@ -42,11 +42,18 @@ public class StudyPlan extends Registry{
                             String mainTask, Integer numberOfSteps, boolean isImportant, LocalDateTime startDate, LocalDateTime endDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
-        this.steps = new ArrayList<>(Arrays.asList(firstStep, resetStudyMechanism, consistentStep, seasonalSteps, basicSteps, "Number of steps: " + numberOfSteps.toString(), "Is it important to you? " + isImportant, startDate.format(formatter), endDate.format(formatter), mainObjectiveTitle, mainGoalTitle, mainMaterialTopic, mainTask));
+        this.steps = new ArrayList<>(Arrays.asList(
+                firstStep, resetStudyMechanism, consistentStep, seasonalSteps, basicSteps,
+                "Number of steps: " + numberOfSteps.toString(),
+                "Is it important to you? " + isImportant,
+                startDate.format(formatter),
+                endDate.format(formatter),
+                mainObjectiveTitle, mainGoalTitle, mainMaterialTopic, mainTask));
     }
 
-    public void handleAssignSteps(List<String> stringProperties, Integer numberOfSteps, boolean isImportant, LocalDateTime startDate, LocalDateTime endDate){
-        assignSteps(stringProperties.get(0), stringProperties.get(1), stringProperties.get(2), stringProperties.get(3), stringProperties.get(4), stringProperties.get(5), stringProperties.get(6), stringProperties.get(7), stringProperties.get(8), numberOfSteps, isImportant, startDate, endDate);
+    public void handleAssignSteps(List<String> stringProperties, Integer numberOfSteps, boolean isImportant, LocalDateTime startDate, LocalDateTime endDate) {
+        assignSteps(stringProperties.get(0), stringProperties.get(1), stringProperties.get(2), stringProperties.get(3), stringProperties.get(4),
+                stringProperties.get(5), stringProperties.get(6), stringProperties.get(7), stringProperties.get(8),
+                numberOfSteps, isImportant, startDate, endDate);
     }
-
 }
